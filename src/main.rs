b67e6066eb3d5c7bd87e76c593f7d7d95a080927
Matt Wells <matt.wells@performancehorizon.com>
2015-04-26 13:39:20 +0100

    Repo Setup

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+extern crate rand;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+
+use rand::Rng;
+
+fn main() {
+    // Generate the markov chain by parsing the corpus
+
+    let path = Path::new("/tmp/alice29.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that describes the error
+        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
+        Ok(_) => print!("File Read.."),
+    };
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
+
+    println!("Generating chain");
+
+    let mut chain = HashMap::new();
+
+    let mut last: &str = "";
+    let mut last_plus: &str = "";
+
+    for word in s.split_whitespace() {
+
+        if word.is_empty() {
+            break;
+        }
+
+        if last.is_empty() == false && last_plus.is_empty() == false {
+            let key = (last_plus, last);
+            let val = match chain.entry(key) {
+                Vacant(entry) => entry.insert(Vec::new()),
+                Occupied(entry) => entry.into_mut(),
+            };
+            val.push(word);
+        }
+        last_plus = last;
+        last = word;
+
+    }
+
+    println!("Generated");
+    println!("{}", format!("{:?}", chain.get(&(("in", "all"))).unwrap().get(0)));
+    let mut rng = rand::thread_rng();
+    let ran = rng.gen_range(0,5);
+    println!("Random: {}", ran);
+
+//    hm.insert(("one","two"), "three");
+//    match hm.get(&(("in", "all"))) {
+//        Some(&name) => println!("== {}", name),
+//        _ => println!("Nope"),
+//    };
+
+
+//            match hm.get(&((last, last_plus))) {
+//                Some(ref mut v) => v.push(word),
+//                _ => {
+//                    let mut v: Vec<&str> = Vec::new();
+//                    v.push(word);
+//                   hm.insert((last, last_plus),v);
+//                }
+//            };
+
+}
